6d0d6b9a48b8c07634605d288886ce01ed98f7f0
Fernando Vargas <fervargas2210@gmail.com>
2026-05-12 00:24:28 -0300

    Conectando front e back a partir da url pública.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({ 
     //baseURL: "http://192.168.68.240:5031/" BRASA CHURCH
-    baseURL: "https://192.168.15.10:7273" //CASA
+    //baseURL: "https://192.168.15.10:7273" //CASA
+    baseURL: import.meta.env.VITE_API_URL
 })
