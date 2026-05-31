5411d87594a259935bf7cbfa3a8aae7912774b11
Fernando Vargas <fervargas2210@gmail.com>
2026-05-31 08:12:13 -0300

    FIX - Ajustado para reimprimir venda.

--- src/routes/vendas.tsx
+++ src/routes/vendas.tsx
@@ -58,7 +58,7 @@ function Vendas() {
 
 function reimprimirVenda(id: number) {
   window.open(
-    `${window.location.origin}/imprimir/reimprimir/${id}`,
+    `${window.location.origin}/imprimir/${id}`,
     "_blank"
   );
 }
